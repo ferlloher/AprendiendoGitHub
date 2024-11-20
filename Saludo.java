5b4cf86aaddf7d214583a55c324f7a950f1211d3
ferlloher <ferlloher@edu.gva.es>
2024-11-20 19:46:12 +0100

    SAludorCOonEdad

--- Saludo.java
+++ Saludo.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
-public class Saludo {
-public void saludar() {
+public void saludarConEdad(){
 Scanner scan = new Scanner(System.in);
 System.out.print("Dime tu nombre: ");
 String nombre = scan.nextLine();
-System.out.println("Hola " + nombre);
+System.out.print("¿Cuántos años tienes?: ");
+int edad = scan.nextInt();
+scan.nextLine();
+if (edad<18){
+System.out.println("Hola " + nombre + " eres bastante joven.");
+}else{
+System.out.println("Hola " + nombre + " espero que tengas un gran día.");
 }
 }
