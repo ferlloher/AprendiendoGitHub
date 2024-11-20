78b2a2988ed02e6838835e0fadf5c0ac5b813988
ferlloher <ferlloher@edu.gva.es>
2024-11-20 19:55:22 +0100

    Despedida modificado 2

--- Despedida.java
+++ Despedida.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class Despedida {
-public void despedir() {
+public void despedir(){
 Scanner scan = new Scanner(System.in);
 System.out.print("Dime tu nombre: ");
 String nombre = scan.nextLine();
-System.out.println("Hasta luego " + nombre);
+System.out.println("Nos vemos " + nombre);
+System.out.println("Hasta luego" + nombre + " .Espero verte más a menudo.");
 }
 }
